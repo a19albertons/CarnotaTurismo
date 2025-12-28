cc9a133df76b7b4d9b682cc892cfe64a38ccce74
A19AlbertoNS <a19albertons@iessanclemente.net>
2025-12-28 20:25:14 +0100

    Completed theoretically Lugar

--- app/src/main/java/com/example/carnotaturismo/fragments/LugarFragment.kt
+++ app/src/main/java/com/example/carnotaturismo/fragments/LugarFragment.kt
@@ -5,56 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.carnotaturismo.R
+import com.example.carnotaturismo.adapters.LugarAdapter
+import com.example.carnotaturismo.adapters.VerTodosAdapter
+import com.example.carnotaturismo.databinding.FragmentLugarBinding
+import com.example.carnotaturismo.model.TurismoAppModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LugarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LugarFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentLugarBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val model: TurismoAppModel by viewModels() {
+        ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lugar, container, false)
+        _binding = FragmentLugarBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Recycler View
+        // Configurar el RecyclerView
+        val recyclerView = binding.lugarAdapter
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val listaLugares = model.lugares.value ?: emptyList()
+        val adapter = LugarAdapter(listaLugares)
+        recyclerView.adapter = adapter
+        model.lugares.observe(viewLifecycleOwner) { lista ->
+            adapter.setData(lista)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LugarFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LugarFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
