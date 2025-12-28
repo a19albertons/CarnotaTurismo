d203d12d8fc94fd9c00c5ec2f08f2863475a44ac
A19AlbertoNS <a19albertons@iessanclemente.net>
2025-12-28 17:02:52 +0100

    Basic functinality RecyclerView nested

---
+++ app/src/main/java/com/example/carnotaturismo/adapters/InicioHorizontalAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.carnotaturismo.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.carnotaturismo.R
+import com.example.carnotaturismo.model.Lugar
+
+class InicioHorizontalAdapter(private var lista: List<Lugar>)
+    : RecyclerView.Adapter<InicioHorizontalAdapter.ViewHolder>() {
+
+    // 1. ViewHolder: Guarda las referencias a las vistas de cada fila
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val titulo: TextView = itemView.findViewById(R.id.titulo)
+    }
+
+    // 2. Crea nuevos ViewHolders (infla el layout de la fila)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_inicio_horizontal, parent, false)
+        return ViewHolder(view)
+    }
+
+    // 3. Reemplaza el contenido de una vista (une los datos a la vista)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = lista.get(position)
+        holder.titulo.text = currentItem.titulo
+
+    }
+
+    // 4. Devuelve el número total de elementos
+    override fun getItemCount() = lista.size
+}
---
+++ app/src/main/java/com/example/carnotaturismo/adapters/InicioVerticalAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.carnotaturismo.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.LiveData
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.carnotaturismo.R
+import com.example.carnotaturismo.model.Lugar
+import com.example.carnotaturismo.model.TipoUbicacion
+
+class InicioVerticalAdapter(private var tipos: Array<TipoUbicacion>, private var model: List<Lugar>)
+    : RecyclerView.Adapter<InicioVerticalAdapter.ViewHolder>() {
+
+
+    // 1. ViewHolder: Guarda las referencias a las vistas de cada fila
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tipoUbicacion: TextView = itemView.findViewById(R.id.tipo)
+        val RecyclerViewHorizontal: RecyclerView = itemView.findViewById(R.id.RecyclerViewHorizontal)
+    }
+
+    // Permite actualizar los datos y refrescar la vista
+    fun setData(newModel: List<Lugar>) {
+        model = newModel
+        notifyDataSetChanged()
+    }
+
+    // 2. Crea nuevos ViewHolders (infla el layout de la fila)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.adapter_inicio_vertical, parent, false)
+        return ViewHolder(view)
+    }
+
+    // 3. Reemplaza el contenido de una vista (une los datos a la vista)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = tipos[position]
+        holder.tipoUbicacion.text = currentItem.name
+
+        // Asegurar que el RecyclerView horizontal tenga un LayoutManager para poder hacer el layout
+        if (holder.RecyclerViewHorizontal.layoutManager == null) {
+            holder.RecyclerViewHorizontal.layoutManager = LinearLayoutManager(holder.itemView.context, LinearLayoutManager.HORIZONTAL, false)
+            holder.RecyclerViewHorizontal.setHasFixedSize(true)
+        }
+
+        // Asignar el adaptador (filtrado por tipo)
+        holder.RecyclerViewHorizontal.adapter = InicioHorizontalAdapter(model.filter { it.tipo == currentItem })
+    }
+
+    // 4. Devuelve el número total de elementos
+    override fun getItemCount() = tipos.size
+}
