c3bd39b8097300b03369697c6337d0b248aa2b5d
A19AlbertoNS <a19albertons@iessanclemente.net>
2025-12-29 12:40:55 +0100

    Completed theoretically mapa general

--- app/src/main/java/com/example/carnotaturismo/fragments/MapaGeneralFragment.kt
+++ app/src/main/java/com/example/carnotaturismo/fragments/MapaGeneralFragment.kt
@@ -5,29 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import com.example.carnotaturismo.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.carnotaturismo.databinding.FragmentLugarBinding
+import com.example.carnotaturismo.databinding.FragmentMapaGeneralBinding
+import com.example.carnotaturismo.model.TurismoAppModel
+import kotlin.getValue
 
 /**
- * A simple [Fragment] subclass.
- * Use the [MapaGeneralFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Clase que representa el fragmento MapaGeneral
  */
 class MapaGeneralFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentMapaGeneralBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    // modelo
+    private val model: TurismoAppModel by viewModels() {
+        ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
     }
 
     override fun onCreateView(
@@ -35,26 +30,35 @@ class MapaGeneralFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mapa_general, container, false)
+        _binding = FragmentMapaGeneralBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Lista de lugares
+        var lugares = model.lugares.value ?: emptyList()
+
+        // Declaramos el listado
+        binding.tituloUbicaciones.text = lugares.joinToString(separator = "\n") { it.titulo }
+
+        // Observamos cambios
+        model.lugares.observe(viewLifecycleOwner) { lista ->
+            binding.tituloUbicaciones.text = lista.joinToString(separator = "\n") { it.titulo }
+        }
+
+        //val ctx = holder.itemView.context
+        //val imgRef = currentItem.imagen
+
+        // extrae "drawable" y "mi_imagen"
+        //val (type, name) = imgRef.removePrefix("@").split('/', limit = 2)
+        //val resId = ctx.resources.getIdentifier(name, type, ctx.packageName)
+        //if (resId != 0) {
+        //    holder.imagen.setImageResource(resId)
+        //} else {
+        //    holder.imagen.setImageResource(R.drawable.error_imagen) // fallback
+        //}
+
+        return view
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapaGeneralFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapaGeneralFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
