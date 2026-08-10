5f8df5fe2c8ee1187a3a61805dde769256cbb974
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-08-10 19:22:58 +0200

    Correción ktlint

--- CarnotaTurismoApp/app/src/main/java/com/example/carnotaturismo/MainActivity.kt
+++ CarnotaTurismoApp/app/src/main/java/com/example/carnotaturismo/MainActivity.kt
@@ -6,8 +6,6 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
